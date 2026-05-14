f51a89fc611b424ee69fa1a10efffcbf41891162
dmitry <dmitryed6263@gmail.com>
2026-05-14 22:57:53 +0400

    feat: added The main class of the creature

---
+++ src/main/java/org/dm/entities/creatures/Creature.java
@@ -0,0 +1,12 @@
+package org.dm.entities.creatures;
+
+import org.dm.entities.Entity;
+
+public class Creature extends Entity {
+
+    private int speed;
+    private int health;
+
+    // в процессе реализации
+    void makeMove(){}
+}
