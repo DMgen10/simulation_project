438f12fd0b8febf7971b095c10754cde8857f4af
dmitry <dmitryed6263@gmail.com>
2026-05-14 23:01:45 +0400

    feat: added the main static object class

---
+++ src/main/java/org/dm/entities/static_object/Inanimate.java
@@ -0,0 +1,8 @@
+package org.dm.entities.static_object;
+
+import org.dm.entities.Entity;
+
+public abstract class Inanimate extends Entity {
+
+    // нужно решить, как реализовать съедобность объекта
+}
