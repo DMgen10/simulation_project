fefb1f2ac39d7bf8d1e0418a53b5dd2ee2e80992
dmitry <dmitryed6263@gmail.com>
2026-05-15 21:31:20 +0400

    feat: added the class of dictionary sprites

---
+++ src/main/java/org/dm/SpriteDictionary.java
@@ -0,0 +1,11 @@
+package org.dm;
+
+public class SpriteDictionary {
+
+    public static final String SPRITE_HERBIVORE = "\uD83D\uDC0F";
+    public static final String SPRITE_PREDATOR = "\uD83D\uDC15";
+    public static final String SPRITE_GRASS = "\uD83C\uDF3F";
+    public static final String SPRITE_ROCK = "\uD83E\uDEA8";
+    public static final String SPRITE_TREE = "\uD83C\uDF32";
+    public static final String SPRITE_EMPTY = " . ";
+}
