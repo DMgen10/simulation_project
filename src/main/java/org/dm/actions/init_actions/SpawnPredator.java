676c55aaf11ae21a744e66334a2697d40da9b1c5
dmitry <dmitryed6263@gmail.com>
2026-05-15 22:52:31 +0400

    feat:added a predator spawn class

---
+++ src/main/java/org/dm/actions/init_actions/SpawnPredator.java
@@ -0,0 +1,11 @@
+package org.dm.actions.init_actions;
+
+import org.dm.entities.Entity;
+import org.dm.entities.creatures.Predator;
+
+public class SpawnPredator extends SpawnAction{
+    @Override
+    protected Entity spawn() {
+        return new Predator();
+    }
+}
