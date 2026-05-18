4509f352a0e6a03425d2c3ae190fec98cb613735
dmitry <dmitryed6263@gmail.com>
2026-05-18 23:02:28 +0400

    feat: added a stub to the makeMove method

--- src/main/java/org/dm/entities/creatures/Creature.java
+++ src/main/java/org/dm/entities/creatures/Creature.java
@@ -1,5 +1,7 @@
 package org.dm.entities.creatures;
 
+import org.dm.MapSimulation;
+import org.dm.Position;
 import org.dm.entities.Entity;
 
 public class Creature extends Entity {
@@ -8,5 +10,10 @@ public class Creature extends Entity {
     private int health;
 
     // в процессе реализации
-    void makeMove(){}
+    public void makeMove(MapSimulation map, Position currentPosition){
+        map.remove(currentPosition);
+        map.add(currentPosition, map.getEntity(currentPosition));
+    }
+
+
 }
