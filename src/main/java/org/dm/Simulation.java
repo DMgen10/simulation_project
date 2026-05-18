ad9a3e889047b29615438eebcef1757fbb9f26a2
dmitry <dmitryed6263@gmail.com>
2026-05-18 23:04:56 +0400

    feat: added the main class Simulation.java

---
+++ src/main/java/org/dm/Simulation.java
@@ -0,0 +1,20 @@
+package org.dm;
+
+public class Simulation {
+
+    /*
+        1. карта
+        2. счетчик ходов
+        3. рендер карты
+        4. actions действий
+
+        два массива:
+        turnActions
+        initActions
+
+        методы:
+        nextTurn() - каждый ход
+        startSimulation() - старт
+        pauseSimulation() - пауза
+     */
+}
