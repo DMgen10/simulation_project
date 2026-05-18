cee515ac6e955e0c7adf0f356aa0203bbc6cc34f
dmitry <dmitryed6263@gmail.com>
2026-05-18 23:07:11 +0400

    feat: added a class using the BFS algorithm

---
+++ src/main/java/org/dm/path_finder/BFS.java
@@ -0,0 +1,14 @@
+package org.dm.path_finder;
+
+import org.dm.MapSimulation;
+import org.dm.Position;
+import org.dm.entities.Entity;
+
+import java.util.List;
+
+public class BFS implements PathFinder{
+    @Override
+    public List<Position> find(MapSimulation map, Position start, Class<? extends Entity> targetType) {
+
+    }
+}
