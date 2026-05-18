193495a2884c8962e9f9d710e9ccdd964d85f6f8
dmitry <dmitryed6263@gmail.com>
2026-05-18 23:06:30 +0400

    feat: added a class for searching for creatures and iterating the makeMove method

---
+++ src/main/java/org/dm/actions/MoveAction.java
@@ -0,0 +1,45 @@
+package org.dm.actions;
+
+import org.dm.MapSimulation;
+import org.dm.Position;
+import org.dm.entities.Entity;
+import org.dm.entities.creatures.Creature;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MoveAction implements Action {
+
+    private MapSimulation map;
+
+    public MoveAction(MapSimulation map) {
+        this.map = map;
+    }
+
+    @Override
+    public void execute(MapSimulation map) {
+        Map<Position, Creature> creaturesForTurn = countingCreatures(map);
+
+        for (Map.Entry<Position,Creature> entry: creaturesForTurn.entrySet()){
+            Position currentPosition = entry.getKey();
+            Creature creature = entry.getValue();
+
+            creature.makeMove(map, currentPosition);
+        }
+    }
+
+    private Map<Position, Creature> countingCreatures(MapSimulation map){
+        Map<Position, Creature> creatures = new HashMap<>();
+        for (int x = 0; x < map.getWidth(); x++) {
+            for (int y = 0; y < map.getHeight(); y++) {
+                Position position = new Position(x,y);
+                Entity entity = map.getEntity(position);
+
+                if (map.getEntity(position) instanceof Creature && entity != null){
+                    creatures.put(position, (Creature) entity);
+                }
+            }
+        }
+        return creatures;
+    }
+}
