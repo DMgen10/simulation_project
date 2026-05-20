b7ddc5a12b311f7dee2eee1f130cad9066d6d388
dmitry <dmitryed6263@gmail.com>
2026-05-20 22:48:44 +0400

    feat: implemented abstract methods for movement and eating

--- src/main/java/org/dm/entities/creatures/Herbivore.java
+++ src/main/java/org/dm/entities/creatures/Herbivore.java
@@ -1,4 +1,28 @@
 package org.dm.entities.creatures;
 
+import org.dm.MapSimulation;
+import org.dm.Position;
+import org.dm.entities.Entity;
+import org.dm.entities.static_object.Grass;
+
 public class Herbivore extends Creature {
+
+    public Herbivore(int speed, int health) {
+        super(2, 20);
+    }
+
+    @Override
+    protected void interactWithTarget(MapSimulation map, Position currentPosition, Position newPosition, Entity entity) {
+        Grass grass = (Grass) entity;
+
+        this.heal(grass.getNutritionValue());
+
+        map.remove(currentPosition);
+        map.add(newPosition,this );
+    }
+
+    @Override
+    protected Class<? extends Entity> getTargetType() {
+        return Grass.class;
+    }
 }
