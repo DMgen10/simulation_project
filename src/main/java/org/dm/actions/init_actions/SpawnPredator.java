274d8916b20f132bddfe3fd8385437e6e6b31aea
dmitry <dmitryed6263@gmail.com>
2026-05-20 22:43:55 +0400

    feat: a temporary solution via the constructor

--- src/main/java/org/dm/actions/init_actions/SpawnPredator.java
+++ src/main/java/org/dm/actions/init_actions/SpawnPredator.java
@@ -6,6 +6,6 @@ import org.dm.entities.creatures.Predator;
 public class SpawnPredator extends SpawnAction{
     @Override
     protected Entity spawn() {
-        return new Predator();
+        return new Predator(1, 20);
     }
 }
