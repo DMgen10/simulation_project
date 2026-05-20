96aac84ea7915a50887d6faf4af754690124dd96
dmitry <dmitryed6263@gmail.com>
2026-05-20 22:44:38 +0400

    feat: a temporary solution via the field

--- src/main/java/org/dm/entities/static_object/Grass.java
+++ src/main/java/org/dm/entities/static_object/Grass.java
@@ -1,4 +1,10 @@
 package org.dm.entities.static_object;
 
 public class Grass extends Inanimate {
+
+    private int nutritionValue = 10;
+
+    public int getNutritionValue() {
+        return nutritionValue;
+    }
 }
