fca069e5949b1acc08336550ce9e635635e624bc
dmitry <dmitryed6263@gmail.com>
2026-05-26 22:52:23 +0400

    feat: improved the class by adding settings SettingsSimulation.java

--- src/main/java/org/dm/actions/init_actions/SpawnTree.java
+++ src/main/java/org/dm/actions/init_actions/SpawnTree.java
@@ -1,9 +1,14 @@
 package org.dm.actions.init_actions;
 
+import org.dm.SettingsSimulation;
 import org.dm.entities.Entity;
 import org.dm.entities.static_object.Tree;
 
 public class SpawnTree extends SpawnAction{
+    public SpawnTree(SettingsSimulation settings) {
+        super(settings, settings.getCountTrees());
+    }
+
     @Override
     protected Entity spawn() {
         return new Tree();
