27b508ee5f2ee320aef835d2c00bd891c31d6a84
dmitry <dmitryed6263@gmail.com>
2026-05-26 22:51:53 +0400

    feat: improved the class by adding settings SettingsSimulation.java

--- src/main/java/org/dm/actions/init_actions/SpawnPredator.java
+++ src/main/java/org/dm/actions/init_actions/SpawnPredator.java
@@ -1,11 +1,17 @@
 package org.dm.actions.init_actions;
 
+import org.dm.SettingsSimulation;
 import org.dm.entities.Entity;
 import org.dm.entities.creatures.Predator;
 
 public class SpawnPredator extends SpawnAction{
+
+    public SpawnPredator(SettingsSimulation settings) {
+        super(settings, settings.getCountPredators());
+    }
+
     @Override
     protected Entity spawn() {
-        return new Predator(1, 20);
+        return new Predator(settings.getSpeedPredator(), settings.getHealthPredator(), settings.getDamage());
     }
 }
