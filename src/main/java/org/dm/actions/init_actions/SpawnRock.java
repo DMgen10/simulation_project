f3adbed072a07fcaaf2ea717e23e33dc89bf62a7
dmitry <dmitryed6263@gmail.com>
2026-05-26 22:52:10 +0400

    feat: improved the class by adding settings SettingsSimulation.java

--- src/main/java/org/dm/actions/init_actions/SpawnRock.java
+++ src/main/java/org/dm/actions/init_actions/SpawnRock.java
@@ -1,9 +1,15 @@
 package org.dm.actions.init_actions;
 
+import org.dm.SettingsSimulation;
 import org.dm.entities.Entity;
 import org.dm.entities.static_object.Rock;
 
 public class SpawnRock extends SpawnAction{
+
+    public SpawnRock(SettingsSimulation settings) {
+        super(settings, settings.getCountRocks());
+    }
+
     @Override
     protected Entity spawn() {
         return new Rock();
