4a91dfd763d6a026ff88a59fdd456d1b15c23594
dmitry <dmitryed6263@gmail.com>
2026-05-26 22:51:40 +0400

    feat: improved the class by adding settings SettingsSimulation.java

--- src/main/java/org/dm/actions/init_actions/SpawnHerbivore.java
+++ src/main/java/org/dm/actions/init_actions/SpawnHerbivore.java
@@ -1,11 +1,17 @@
 package org.dm.actions.init_actions;
 
+import org.dm.SettingsSimulation;
 import org.dm.entities.Entity;
 import org.dm.entities.creatures.Herbivore;
 
 public class SpawnHerbivore extends SpawnAction{
+
+    public SpawnHerbivore(SettingsSimulation settings) {
+        super(settings, settings.getCountHerbivores());
+    }
+
     @Override
     protected Entity spawn() {
-        return new Herbivore(2, 20);
+        return new Herbivore(settings.getSpeedHerbivore(), settings.getHealthHerbivore());
     }
 }
