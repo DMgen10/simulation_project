cd03aa68851a31782074c691952ab958472f41c8
dmitry <dmitryed6263@gmail.com>
2026-05-26 22:53:57 +0400

    feat: updated the value input class

--- src/main/java/org/dm/Input.java
+++ src/main/java/org/dm/Input.java
@@ -7,17 +7,24 @@ public class Input {
     private final Scanner scanner = new Scanner(System.in);
 
     public int inputValue(String value){
-        System.out.println("Введите значение для параметра: " + value);
-        while (scanner.hasNext()){
-            int input = scanner.nextInt();
-            if (input > 0){
-//                System.out.println("Выбрано значение: " + input);
-                return input;
-            } else {
+        while (true){
+            System.out.println("Введите значение для параметра: " + value);
+
+            if (scanner.hasNextInt()){
+                int input = scanner.nextInt();
+
+                scanner.nextLine();
+
+                if (input > 0){
+                System.out.println("Выставлено значение для параметра \"" + value + "\": " + input);
+                    return input;
+                } else {
                 System.out.println("Значение должно быть больше нуля");
             }
-
+        } else {
+                String incorrectInput = scanner.next();
+                System.out.println(incorrectInput + " не является целым числом");
+            }
         }
-        throw new IllegalArgumentException();
     }
 }
