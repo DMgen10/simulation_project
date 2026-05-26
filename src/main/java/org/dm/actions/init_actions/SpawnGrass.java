cc082d6a6b5d7caa3c201058faf62c18d55827bf
dmitry <dmitryed6263@gmail.com>
2026-05-26 22:50:38 +0400

    feat: improved the class by adding settings SettingsSimulation.java

--- src/main/java/org/dm/actions/init_actions/SpawnGrass.java
+++ src/main/java/org/dm/actions/init_actions/SpawnGrass.java
@@ -1,11 +1,17 @@
 package org.dm.actions.init_actions;
 
+import org.dm.SettingsSimulation;
 import org.dm.entities.Entity;
 import org.dm.entities.static_object.Grass;
 
 public class SpawnGrass extends SpawnAction {
+
+    public SpawnGrass(SettingsSimulation settings) {
+        super(settings, settings.getCountGrass());
+    }
+
     @Override
     protected Entity spawn() {
-        return new Grass();
+        return new Grass(settings.getNutritionalValue());
     }
 }
