ad26029f9760a2dda364f7fbdf5612fe20759991
dmitry <dmitryed6263@gmail.com>
2026-05-27 21:13:39 +0400

    feat: the class is completed

--- src/main/java/org/dm/simulation/actions/init_actions/SpawnRock.java
+++ src/main/java/org/dm/simulation/actions/init_actions/SpawnRock.java
@@ -1,8 +1,8 @@
-package org.dm.actions.init_actions;
+package org.dm.simulation.actions.init_actions;
 
-import org.dm.SettingsSimulation;
-import org.dm.entities.Entity;
-import org.dm.entities.static_object.Rock;
+import org.dm.simulation.SettingsSimulation;
+import org.dm.simulation.entities.Entity;
+import org.dm.simulation.entities.static_object.Rock;
 
 public class SpawnRock extends SpawnAction{
 
