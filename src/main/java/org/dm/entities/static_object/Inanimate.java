78fac205277b57c325fb9748318e889fd78af252
dmitry <dmitryed6263@gmail.com>
2026-05-27 12:26:13 +0400

    feat: delete comment

--- src/main/java/org/dm/entities/static_object/Inanimate.java
+++ src/main/java/org/dm/entities/static_object/Inanimate.java
@@ -3,6 +3,4 @@ package org.dm.entities.static_object;
 import org.dm.entities.Entity;
 
 public abstract class Inanimate extends Entity {
-
-    // нужно решить, как реализовать съедобность объекта
 }
