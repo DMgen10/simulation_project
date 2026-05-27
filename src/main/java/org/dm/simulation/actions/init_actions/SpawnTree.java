0f9090352f0309ce719b5aef87784c2a2ccf6342
dmitry <dmitryed6263@gmail.com>
2026-05-27 21:13:30 +0400

    feat: the class is completed

--- src/main/java/org/dm/simulation/actions/init_actions/SpawnTree.java
+++ src/main/java/org/dm/simulation/actions/init_actions/SpawnTree.java
@@ -1,11 +1,12 @@
-package org.dm.actions.init_actions;
+package org.dm.simulation.actions.init_actions;
 
-import org.dm.SettingsSimulation;
-import org.dm.entities.Entity;
-import org.dm.entities.static_object.Tree;
+import org.dm.simulation.SettingsSimulation;
+import org.dm.simulation.entities.Entity;
+import org.dm.simulation.entities.static_object.Tree;
 
 public class SpawnTree extends SpawnAction{
     public SpawnTree(SettingsSimulation settings) {
+
         super(settings, settings.getCountTrees());
     }
 
